78f91004b0a01610f00116e0dc183436e80ec2f7
Samoylenko Dmitry <107771156+dmytrik@users.noreply.github.com>
2022-07-19 08:01:31 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,13 +1,39 @@
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import './css/styles.css';
 import fetchCountries from './fetchCountries';
 const DEBOUNCE_DELAY = 300;
 const search = document.querySelector('#search-box');
-console.log(search);
+const countriesList = document.querySelector('.country-list');
 search.addEventListener('input', debounce(searchCountry, DEBOUNCE_DELAY));
 function searchCountry(event) {
   if (event.target.value.trim() === '') {
     return;
   }
-  fetchCountries(event.target.value.trim());
+  fetchCountries(event.target.value.trim()).then(checkCount);
+}
+function checkCount(response) {
+  console.log(response);
+  if (response.length > 10) {
+    Notiflix.Notify.info(
+      'Too many matches found. Please enter a more specific name.'
+    );
+    return;
+  }
+  if (2 <= response.length && response.length <= 10) {
+    countriesList.innerHTML = '';
+    const countriesItem = response
+      .map(({ flags, name }) => {
+        return `<li class='country-item'>
+        <div class='country-icon'>
+         <img src = ${flags.svg} alt = ${name.common} />
+         </div>
+         <p class ='country-name'> ${name.common} </p>
+      </li>`;
+      })
+      .join('');
+
+    countriesList.insertAdjacentHTML('beforeend', countriesItem);
+    return;
+  }
 }
